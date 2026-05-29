88803669677fa89a95308c177a32f7232084bce9
SATORU <emmanuelstephane671@gmail.com>
2026-05-29 02:43:23 +0100

    Refactor AppServiceProvider structure and methods

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,35 +1,24 @@
-<?php
+ <?php
 
-namespace App\Providers;
+  namespace App\Providers;
 
-use Illuminate\Support\ServiceProvider;
-use Illuminate\Support\Facades\View;
-use Illuminate\Support\Facades\URL;
+  use Illuminate\Support\Facades\URL;
+  use Illuminate\Support\Facades\View;
+  use Illuminate\Support\ServiceProvider;
 
-class AppServiceProvider extends ServiceProvider
-{
-    /**
-     * Register any application services.
-     */
-    public function register(): void
-    {
-        //
-    }
-
-    /**
-     * Bootstrap any application services.
-     */
-    // public function boot(): void
-    // {
-    //     view::share('appName', 'Hack App');
-    // }
-   {
-      if (app()->environment('production')) {
-          URL::forceScheme('https');
+  class AppServiceProvider extends ServiceProvider
+  {
+      public function register(): void
+      {
+          //
       }
 
-      view::share('framework', 'Laravel');
+      public function boot(): void
+      {
+          if (app()->environment('production')) {
+              URL::forceScheme('https');
+          }
+
+          View::share('framework', 'Laravel');
+      }
   }
-
-
-}
